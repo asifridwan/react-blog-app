74076f0449db02b8bdaebd42701869aea541fdae
Asif Ridwan <asifridwan@gmail.com>
2022-04-21 12:00:24 +0600

    Implemented Nested Comments Feature

--- frontend/src/components/CommentCard.jsx
+++ frontend/src/components/CommentCard.jsx
@@ -1,10 +1,59 @@
-export default function CommentCard({id, type, user, time, body}) {
+export default function CommentCard({info, type}) {
+  const comments = [
+    {
+      id: 1,
+      author: 'Name 1',
+      body: 'Body',
+      date: '2022-04-21',
+      parent: null
+    },
+    {
+      id: 2,
+      author: 'Name 2',
+      body: 'Body',
+      date: '2022-04-21',
+      parent: null
+    },
+    {
+      id: 3,
+      author: 'Name 3',
+      body: 'Body',
+      date: '2022-04-21',
+      parent: 1
+    },
+    {
+      id: 4,
+      author: 'Name 4',
+      body: 'Body',
+      date: '2022-04-21',
+      parent: 1
+    },
+    {
+      id: 5,
+      author: 'Name 5',
+      body: 'Body',
+      date: '2022-04-21',
+      parent: 3
+    }
+  ];
+  
+  const replies = comments.filter(reply => reply.parent === info.id)
+
   return (
-    <div className={`comment-card ${type}`}>
-      <div className='comment-username'>{user}</div>
-      <div className='comment-time'>Posted on : {time}</div>
-      <p className='comment-body'>{body}</p>
-      <div className='reply-option'>Reply</div>
+    <div className='comments-container'>
+      <div className={`comment-card ${type}`}>
+        <div className='comment-username'>{info.author}</div>
+        <div className='comment-time'>Posted on : {info.date}</div>
+        <p className='comment-body'>{info.body}</p>
+        <div className='reply-option'>Reply</div>
+      </div>
+      {replies.length > 0 && <div>
+        <div className='replies'>
+          {replies.map((reply, i) => {
+            return <CommentCard key={i} info={reply} type='reply' />
+          })}
+        </div>
+      </div>}
     </div>
   )
 }
--- frontend/src/containers/CommentsList.jsx
+++ frontend/src/containers/CommentsList.jsx
@@ -1,12 +1,39 @@
 import CommentCard from '../components/CommentCard';
 
 export default function CommentsList() {
+  const comments = [
+    {
+      id: 1,
+      author: 'Name 1',
+      body: 'Body',
+      date: '2022-04-21',
+      parent: null
+    },
+    {
+      id: 2,
+      author: 'Name 2',
+      body: 'Body',
+      date: '2022-04-21',
+      parent: null
+    },
+    {
+      id: 3,
+      author: 'Name 3',
+      body: 'Body',
+      date: '2022-04-21',
+      parent: 1
+    }
+  ];
+
+  const rootComments = comments.filter(comment => comment.parent === null);
+
+  const displayComments = rootComments.map((comment, i) => {
+    return <CommentCard key={i} givenWidth={100} info={comment} />
+  });
+
   return (
-    <div className='comments-container'>
-      <CommentCard id={1} user='Ador' time='2022-04-22' body='Good for you' type='parent' />
-      <CommentCard id={1} user='Ador' time='2022-04-22' body='Good for you' type='reply' />
-      <CommentCard id={1} user='Ador' time='2022-04-22' body='Good for you' type='reply' />
-      <CommentCard id={1} user='Ador' time='2022-04-22' body='Good for you' type='parent' />
+    <div className='comment-section'>
+      {displayComments}
     </div>
   )
 }
