a392d007d303f58922ec23d79adf6a17d153e509
Asif Ridwan <asifridwan@gmail.com>
2022-04-23 19:45:05 +0600

    Fixed time formatting problem

--- frontend/src/pages/Blog.jsx
+++ frontend/src/pages/Blog.jsx
@@ -41,6 +41,20 @@ export default function Blog() {
     setReplyID(id);
   }
 
+  function GenerateTimestampWithUTCOffset(date) {
+    const tzOffset = -date.getTimezoneOffset();
+    const diff = tzOffset >= 0 ? '+' : '-';
+    const pad = n => `${Math.floor(Math.abs(n))}`.padStart(2, '0');
+    return date.getFullYear() +
+      '-' + pad(date.getMonth() + 1) +
+      '-' + pad(date.getDate()) +
+      'T' + pad(date.getHours()) +
+      ':' + pad(date.getMinutes()) +
+      ':' + pad(date.getSeconds()) +
+      diff + pad(tzOffset / 60) +
+      ':' + pad(tzOffset % 60);
+  }
+
   function ResetComment() {
     setAuthor('');
     setCommentBody('');
@@ -48,7 +62,7 @@ export default function Blog() {
   }
 
   function PostComment() {
-    const comment_date = new Date().toLocaleString();
+    const comment_date = GenerateTimestampWithUTCOffset(new Date());
 
     axios.post('http://localhost:4000/add/comment', {
       author: author,
